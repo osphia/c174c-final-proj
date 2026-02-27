fa676e816260f95b03409dd04fe26da61bb84e9d
alexhyoon <yoon.h.alex@gmail.com>
2026-02-26 18:11:10 -0800

    creatures and curves

--- underwater/index.js
+++ underwater/index.js
@@ -3,3 +3,4 @@ export {Seafloor} from './environment.js';
 export {Coral_Collection} from './coral.js';
 export {Underwater_Shader} from './shaders.js';
 export {Bubble_System, Plankton_System} from './particles.js';
+export {Jellyfish_School, Sea_Turtle, Manta_Ray} from './creatures.js';
